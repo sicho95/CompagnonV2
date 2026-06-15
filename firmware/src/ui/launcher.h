a4c28a43c19fe6a5f792dfa504a16a168893e713
sicho95 <sicho95@users.noreply.github.com>
2026-06-15 22:15:44 +0200

    firmware: add launcher touch API

--- firmware/src/ui/launcher.h
+++ firmware/src/ui/launcher.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <stdint.h>
+
 void ui_launcher_init();
 void ui_launcher_show();
 void ui_launcher_btn_tick();
+void ui_launcher_touch(bool pressed, uint16_t x, uint16_t y);
