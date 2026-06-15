ac876c588ec0705b63d0c2df212e87e67def3d63
sicho95 <sicho95@users.noreply.github.com>
2026-06-15 22:54:09 +0200

    firmware: add shared app close header

---
+++ firmware/src/ui/app_header.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <lvgl.h>
+
+void ui_app_header_attach(lv_obj_t* screen);
+int  ui_app_header_bar_y();
+int  ui_app_header_bar_h();
+int  ui_app_content_top();
+int  ui_app_content_height();
