4f520a7ad1fc55a6568c14921b90f9bdf7188cb5
sicho95 <sicho95@users.noreply.github.com>
2026-06-16 14:26:36 +0200

    firmware: add structured touch frame layer

--- firmware/src/hal/touch.h
+++ firmware/src/hal/touch.h
@@ -9,9 +9,30 @@
 
 namespace hal {
 
+constexpr uint8_t TOUCH_MAX_POINTS = 5;
+
+struct TouchPoint {
+    bool     valid;
+    int16_t  raw_x;
+    int16_t  raw_y;
+    uint16_t x;
+    uint16_t y;
+};
+
+struct TouchFrame {
+    bool     pressed;
+    bool     just_pressed;
+    bool     just_released;
+    uint8_t  point_count;
+    uint32_t timestamp_ms;
+    TouchPoint points[TOUCH_MAX_POINTS];
+};
+
 bool touch_init();
+bool touch_update();
 bool touch_read(uint16_t& x, uint16_t& y);
 bool touch_has_data();
+const TouchFrame& touch_frame();
 
 } // namespace hal
 
